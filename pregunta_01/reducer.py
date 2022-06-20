1bc6a712a4fdb257671661e15c70da3b0ff1dfc3
caauribegu <caauribegu@unal.edu.co>
2022-06-20 11:29:58 -0500

    PruebaFinal

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,31 @@
-#
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    # Se crea una lista vacia para los tipo de atributos
+    anteratributo = None
+    total = 0
+
+    # Se lee el archivo de entrada linea a linea
+    for line in sys.stdin:
+
+        # se obtiene el atribito de tipo de credito y el valor 1
+        atributo, valor = line.split("\t")
+        # Se convierte el valor a entero
+        valor = int(valor)
+
+        # se compara el atributo si son iguales se le suma 1 al valor
+        if atributo == anteratributo:
+            total += valor
+        else:
+            # se el anterior atributo no es vacio se inmprime la linea
+            if anteratributo is not None:
+                sys.stdout.write("{}\t{}\n".format(anteratributo, total))
+
+            # se reinicia los valores de atributo y valor con el nuevo atributo
+            anteratributo = atributo
+            total = valor
+
+    sys.stdout.write("{}\t{}\n".format(anteratributo, total))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,16 @@
+import sys
+13 lines(11 sloc)  434 Bytes
+
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+
+    # Se lee el archivo linea line.
+    for line in sys.stdin:
+        # se divide la linea del archivo por coma (" ")
+        lista = line.split(",")
+        # se escribe la salida del archivo obteniendo dos columna
+        sys.stdout.write("{}   {}   {}\n".format(
+            lista[0].strip(), lista[2].strip(), int(lista[1].strip())))
